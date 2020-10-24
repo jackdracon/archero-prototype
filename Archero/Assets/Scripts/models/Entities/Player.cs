86a6ec88f6b4152816dcfe839290b666e6905243
Vinicius Bruno <vinicius@hiperverso.com>
2020-10-23 21:36:47 -0300

    InputReader
    [ADD]
    - InputReader object
    - GetClickPosition on Player

--- Archero/Assets/Scripts/models/Entities/Player.cs
+++ Archero/Assets/Scripts/models/Entities/Player.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 /// <summary>
 /// Player's value 
 /// </summary>
+[RequireComponent(typeof(InputReader))]
 public class Player : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private InputReader input;
+
+    private void Awake()
     {
-        
+        input = GetComponent<InputReader>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var _position = input.GetClickPosition();
+        if (_position != null)
+        {
+            Debug.Log("Pos Received " + _position.Value);
+            this.transform.SetPositionAndRotation(_position.Value, Quaternion.identity);
+        }
+            
     }
 }
---
+++ Archero/Assets/Scripts/models/Input/InputReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReader : MonoBehaviour
+{
+    public Vector3? GetClickPosition()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit _hit;
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                return _hit.point;
+            }
+        }
+
+        return null;
+    }
+}
